796fc7fab5fc88a920ae5837b88820ad6435c320
sudakov <vsudakov@bk.ru>
2016-11-18 20:08:33 +0300

    test data

--- fuzzy_preferences.rb
+++ fuzzy_preferences.rb
@@ -4,20 +4,48 @@ require 'json'
 
 file = %{
 {
+  "fuzzy_sets": [
+    {
+        "name": "about 1",
+        "polyline": [[0,0],[1,1],[2.5,0]]
+    },
+    {
+        "name": "about 2",
+        "polyline": [[0.5,0],[2,1],[3.5,0]]
+    },
+    {
+        "name": "big num",
+        "polyline": [[2,0],[10,1]]
+    },
+    {
+        "name": "from 1 to 3",
+        "polyline": [[0.99,0],[1,1],[3,1],[3.01,0]]
+    },
+    {
+        "name": "good",
+        "polyline": [[3,0],[5,1]]
+    },
+    {
+        "name": "bad",
+        "polyline": [[0,0],[1,1],[2,1],[4,0]]
+    },
+    {
+      "name": "red",
+      "membership": [["red",1]]
+    },
+    {
+      "name": "green",
+      "membership": [["green",1]]
+    }
+  ],
   "criterions" : [
     {
       "name": "crit1",
-      "scale": [
-        0,
-        0.2,
-        0.6,
-        0.6,
-        1
-      ]
+      "scale": ["about 1", "about 2", "big num"]
     },
     {
       "name": "crit2",
-      "scale": [1,2,3,4]
+      "scale": ["about 1","from 1 to 3"]
     },
     {
       "name": "crit3",
@@ -25,22 +53,21 @@ file = %{
     },
     {
       "name": "generalized criterion",
-      "scale": ["good","bad"]
-    }  
-  ],
-  "aggregation": [
-    {
-      "input": [0,1,2],
-      "output": 3,
+      "scale": ["bad","good"],
       "rules": [
-        { "conditions": [[0,0],[1,0],[2,1]],
-          "result": 0
+        { "conditions": {"crit1": "about 1", "crit2": "about 1", "crit3":["green","red"] },
+          "result": "bad"
         },
-        { "conditions": [[0,1],[1,1],[2,1]],
-          "result": 2
+        { "conditions": {"crit1": "about 2", "crit2": "from 1 to 3", "crit3":["red"] },
+          "result": "good"
         }
-      ] 
+      ]
     }
+  ],
+  "alternatives" : [
+    {"crit1": 1.5, "crit2": 0.1, "crit3": "green"},
+    {"crit1": 4,   "crit2": 4,   "crit3": "red"},
+    {"crit1": 1,   "crit2": 1,   "crit3": "green"}
   ]
 }
 }
