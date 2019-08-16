dba968cc54aa6cd2e3de552b598a6fad19d28f21
Vladimir Sudakov <vsudakov@bk.ru>
2019-08-17 00:32:20 +0300

    operations

--- python/fuzzy_number.py
+++ python/fuzzy_number.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pandas import DataFrame
 
 class FuzzyNumber:
     def __init__(self, polyline):
@@ -29,3 +30,74 @@ class FuzzyNumber:
 
         a,b = self.get_ab( self.polyline[inds[0][0]], self.polyline[inds[0][0]+1] )
         return (a * x + b)
+
+    def prep_scale(self, other):
+        scale = np.union1d(self.polyline[:,0], other.polyline[:,0])
+        scale_add = []
+        for x1, x2 in zip(scale[:-1],scale[1:]):
+            ys1 = self.get_membership(x1)
+            ys2 = self.get_membership(x2)
+            yo1 = other.get_membership(x1)
+            yo2 = other.get_membership(x2)
+            if (ys1 > yo1 and ys2 < yo2) or (ys1 < yo1 and ys2 > yo2):
+                a_s,b_s = self.get_ab( [x1,ys1], [x2,ys2] )
+                a_o,b_o = self.get_ab( [x1,yo1], [x2,yo2] )
+                scale_add.append(round((b_o-b_s)/(a_s-a_o),5))
+        return np.union1d(scale,scale_add)
+
+    def __eq__(self, other):
+        scale = np.union1d(self.polyline[:,0], other.polyline[:,0])
+        return 1.0 - max(abs(self.get_membership(x) - other.get_membership(x)) for x in scale)
+
+    def __ge__(self, other):
+        scale = self.prep_scale(other)
+        mh = np.array(np.meshgrid(scale,scale)).T.reshape(-1,2)
+        return max( min(self.get_membership(x[0]),other.get_membership(x[1]))
+                    for x in mh if x[0] >= x[1] )
+
+    def __le__(self, other):
+        return other >= self
+
+    def general_operation(self, other,operation):
+        scale = self.prep_scale(other)
+        mh = np.array(np.meshgrid(scale,scale)).T.reshape(-1,2)
+        y = np.round(operation(mh[:,0],mh[:,1]),5)
+        mu = [ min(self.get_membership(x[0]),other.get_membership(x[1]))
+                    for x in mh ]
+        df = DataFrame(dict(y=y, mu=mu))
+        s = df.groupby('y')['mu'].max()
+        a = self.polyline[0][0]
+        b = self.polyline[-1][0]
+        for yy in s.index:
+            if yy == 0 and operation == np.divide:
+                continue
+            for s1 in np.linspace(a,b,1000):
+                if s1 == 0 and operation == np.multiply:
+                    continue
+                s2 = {
+                    np.multiply: yy / s1,
+                    np.add: yy - s1,
+                    np.subtract: s1 - yy,
+                    np.divide: s1 / yy
+                }[operation]
+                m = min(self.get_membership(s1),other.get_membership(s2))
+                if s[yy] < m:
+                    s[yy] = m
+        return FuzzyNumber(list(zip(s.index,s.values)))
+
+    def __add__(self,other):
+        return self.general_operation(other, np.add)
+
+    def __sub__(self,other):
+        return self.general_operation(other, np.subtract)
+
+    def __mul__(self,other):
+        return self.general_operation(other, np.multiply)
+
+    def __truediv__(self,other):
+        return self.general_operation(other, np.divide)
+
+    def __repr__(self):
+        spolyline = repr(self.polyline)
+        spolyline = spolyline[spolyline.find('['):-1]
+        return f"FuzzyNumber({spolyline})"
--- python/test_fuzzy.py
+++ python/test_fuzzy.py
@@ -1,6 +1,17 @@
-import fuzzy_number
-       
-num1 = FuzzyNumber([[1,0],[2,1],[3.1,0]])
-num2 = FuzzyNumber([[1.7,0],[3,1],[4,0]])
+import fuzzy_number as fz
+
+num1 = fz.FuzzyNumber([[1,0],[2,1],[3.1,0]])
+num2 = fz.FuzzyNumber([[1.7,0],[3,1],[4,0]])
 
 print(num1.get_membership(1.5))
+
+print(num1 == num2)
+
+print(num1 >= num2)
+
+print(num1 <= num2)
+
+print(num1 * num2)
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
